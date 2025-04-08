fd1bca4cbd0f60c492b56e8d2714429f982572f8
Syllas <syllas500@gmail.com>
2025-04-07 22:32:08 -0300

    finalizando meu projeto blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: ' saiu novo filme',
+    descripition: 'ola',
+    photoCover: '',
+  },
+  {
+    id: '2',
+    title: ' saiu novo filme',
+    descripition: 'Novo filme do Homem de Ferro',
+    photoCover: '',
+  },
+];
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+  @Input()
+  photoCover: string =
+    'https://ofolhetimcultural.com.br/wp-content/uploads/2024/05/Universo-Cinematografico-da-Marvel.jpg';
+  @Input()
+  contentTitle: string = 'Minha noticia';
+  @Input()
+  contentDescripition: string = 'Bla blablbablalbablablalbalblabl';
+  private id: string | null = '0';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id.toString() == id)[0];
+    this.contentTitle = result.title;
+    this.contentDescripition = result.descripition;
+    this.photoCover = result.photoCover;
+  }
 }
